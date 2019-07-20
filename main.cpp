e2a696d51e249b0c48af5cf775940874244ac50c
Atirath Kosireddy <atikosireddy98@gmail.com>
2019-07-19 17:39:34 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,66 @@
+//// $Id: main.cpp,v 1.11 2018-01-25 14:19:29-08 - - $
+//
+//#include <cstdlib>
+//#include <exception>
+//#include <iostream>
+//#include <string>
+//#include <unistd.h>
+//#include <cassert>
+//
+//using namespace std;
+//
+//#include "listmap.h"
+//#include "xpair.h"
+//#include "util.h"
+//#include <regex>
+//
+//
+//using str_str_map = listmap<string, string>;
+//using str_str_pair = str_str_map::value_type;
+//
+////void scan_options(int argc, char** argv) {
+////     opterr = 0;
+////     for (;;) {
+////          int option = getopt(argc, argv, "@:");
+////          if (option == EOF) break;
+////          switch (option) {
+////          case '@':
+////               debugflags::setflags(optarg);
+////               break;
+////          default:
+////               complain() << "-" << char(optopt) << ": invalid option"
+////                    << endl;
+////               break;
+////          }
+////     }
+////}
+//
+//int main(int argc, char** argv) {
+//     sys_info::execname(argv[0]);
+//     //scan_options(argc, argv);
+//     str_str_map map;
+//     regex comment_regex{ R"(^\s*(#.*)?$)" };
+//     regex key_value_regex{ R"(^\s*(.*?)\s*=\s*(.*?)\s*$)" };
+//     regex trimmed_regex{ R"(^\s*([^=]+?)\s*$)" };
+//     for (;;) {
+//          string line;
+//          getline(cin, line);
+//          if (cin.eof()) break;
+//          cout << endl << "input: \"" << line << "\"" << endl;
+//          smatch result;
+//          if (regex_search(line, result, comment_regex)) {
+//               continue;
+//          }
+//          if (regex_search(line, result, key_value_regex)) {
+//               auto i = map.find(result[1]);
+//               cout << "key  : \"" << result[1] << "\"" << endl;
+//               cout << "value: \"" << result[2] << "\"" << endl;
+//          }
+//          else if (regex_search(line, result, trimmed_regex)) {
+//               cout << "query: \"" << result[1] << "\"" << endl;
+//          }
+//     }
+//     cout << "EXIT_SUCCESS" << endl;
+//     return EXIT_SUCCESS;
+//}
+//
---
+++ testClient.cpp
@@ -0,0 +1,21 @@
+// $Id: main.cpp,v 1.11 2018-01-25 14:19:29-08 - - $
+
+#include <cstdlib>
+#include <exception>
+#include <iostream>
+#include <string>
+#include <unistd.h>
+
+using namespace std;
+
+#include "listmap.h"
+#include "xpair.h"
+
+// using str_str_map = listmap<string, string>;
+// using str_str_pair = str_str_map::value_type;
+
+int main(int argc, char** argv) {
+     cout << "hello";
+     return 0;
+}
+
